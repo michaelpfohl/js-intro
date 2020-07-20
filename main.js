00ef1964b4fe4a3fc0bb3cdd7ce0a46aa252fc39
Michael Pfohl <rmpfohl@gmail.com>
2020-07-20 18:52:17 -0500

    test data types

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("main.js connected!")
+// * undefined
+// let x;
+// console.log(x);
+
+// * Boolean
+// let x = true;
+// let y = false;
+// console.log(x,y);
+
+// if (!x) {
+//     console.log("This is true")
+// } else {
+//     console.log("This is false")
+// }
+
+// * Number
+// let number = '22';
+// console.log(Number(number) + 22);
+
+// * String
+// let string = 'Today is Monday';
+
+// if (string === 'Today is Monday'){
+//     console.log("YAYAYAY!")
+// }
+
+// console.log(string);
+
+let array = [];
+let objest = {}
+
+console.log(typeof object);
