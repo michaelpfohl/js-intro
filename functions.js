d3e46802a2f162c6c2a5c70f69cb0e0abf99f152
Michael Pfohl <rmpfohl@gmail.com>
2020-07-20 19:57:03 -0500

    add basic functions

---
+++ functions.js
@@ -0,0 +1,18 @@
+const nuggetizer = (animal) => {
+    return `processed ${animal}`;
+}
+
+console.log(nuggetizer("seitan"));
+console.log(nuggetizer("student"));
+
+const addMeaning = (num) => {
+    return num + 42;
+}
+
+console.log(addMeaning(1));
+
+const ageInYear = (born, year) => {
+    return year - born;
+}
+
+console.log(ageInYear(1992, 2099));
